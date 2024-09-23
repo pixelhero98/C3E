c59a2743a8933be97374875de218e5953e148367
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2024-09-23 13:41:33 +0100

    Add files via upload

---
+++ graph_nn_optimization.py
@@ -0,0 +1,124 @@
+import torch
+import numpy as np
+from scipy.optimize import minimize
+
+
+class GraphNNOptimization:
+    def __init__(self, data):
+
+        self.data = data
+        self.N, self.M = data.x.shape
+        self.edge_index, self.edge_attr = data.edge_index, data.edge_attr
+
+    def edge_list_to_adjacency_matrix(self):
+
+        adj = torch.zeros((self.N, self.N), dtype=torch.float32)
+        if self.edge_attr is None:
+            attr = torch.ones(self.edge_index.shape[1], dtype=torch.float32)
+            adj[self.edge_index[0], self.edge_index[1]] = attr
+            adj[self.edge_index[1], self.edge_index[0]] = attr
+        else:
+            attr = self.edge_attr
+            adj[self.edge_index[0], self.edge_index[1]] = attr
+            adj[self.edge_index[1], self.edge_index[0]] = attr
+
+        return adj
+
+    def dropouts(self, layers):
+
+        p = []
+        for idx in range(len(layers) - 1):
+            w0, w1 = layers[idx], layers[idx + 1]
+            c = (w0 * w1) / (w0 + w1)
+            p.append(1 - (c / w1))
+
+        return p
+
+    def aspect_ratio(self, layers):
+
+        if len(layers) <= 1:
+            raise ValueError("Aspect ratio requires at least two layers (excluding the input layer).")
+        prod = np.prod(layers)
+        depth = len(layers)
+        geo_mean = prod ** (1 / depth)
+
+        return depth / geo_mean
+
+    def calculate_average_node_degree(self):
+
+        total_edges = self.data.num_edges
+        num_nodes = self.N
+        if num_nodes == 0:
+            raise ValueError("The graph must contain at least one node.")
+
+        return total_edges / num_nodes
+
+    def regularized_feature_dim(self):
+
+        if self.M == 0:
+            raise ValueError("The number of features (M) must be greater than 0.")
+        inv_avg_degree = 1/self.calculate_average_node_degree()
+        reg_m = self.M * ((1/inv_avg_degree) ** inv_avg_degree)
+
+        return reg_m
+
+    def objective(self, x):
+
+        reg_m = self.regularized_feature_dim()
+        d = self.calculate_average_node_degree()
+        w = x
+        psi = 100000
+
+        if any(w_i > reg_m for w_i in w):
+            return psi
+
+        return -0.5 * (np.log(2 * np.pi * np.exp(1)) + np.sum(np.log(w[:-1]/d)) + np.log(w[-1]))
+
+    def constraint(self, x, H):
+
+        w = x
+        terms = [np.log(self.M * w[0] / (self.M + w[0]))]
+
+        for i in range(1, len(w)):
+            terms.append((1 / (i + 1)) * np.log(w[i - 1] * w[i] / (w[i - 1] + w[i])))
+
+        return sum(terms) - H
+
+    def optimize_weights(self, H, verbose=False):
+
+        L = 2  # Initialize with at least 2 message-passing layers
+
+        while True:
+            w_initial = np.ones(L)  # Initialize widths for these layers
+            bounds = [(1, (self.N - 1) * self.M)]  # Bounds for weights
+
+            # Perform the optimization given L
+            constraints = [{'type': 'ineq', 'fun': lambda w: self.constraint(w, H)}]
+            result = minimize(lambda w: self.objective(w), w_initial, method='SLSQP', bounds=bounds,
+                              constraints=constraints)
+
+            if result.success:
+                current_constraints = self.constraint(result.x, H)
+
+                if verbose:
+                    widths = result.x
+                    entropy = -self.objective(result.x)
+                    self.print_optimization_results(L, widths, entropy, current_constraints, H)
+
+                if current_constraints > 1.3 * H:
+                    break
+            else:
+                print("Optimization failed for L =", L, ". Please check the constraints and initial conditions.")
+
+            # Increment L and try for next depth
+            L += 1
+
+    def print_optimization_results(self, L, weights, current_entropy, current_constraints, H):
+        layers = [self.M] + weights.tolist()
+        print(f"Number of Message-passing Layers: {L}")
+        print(f"Weights: {weights.tolist()}")
+        print(f"Current Model Entropy: {current_entropy}")
+        print(f"Current Constraints/Maximum Graph Entropy: {current_constraints}/{H}")
+        print(f"Dropout Probabilities: {self.dropouts(layers)}")
+        print(f"Aspect Ratio: {self.aspect_ratio(layers)}")
+        print("\n")
