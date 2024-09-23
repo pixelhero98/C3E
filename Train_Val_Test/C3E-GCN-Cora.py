2bebbb9fedd83a7a204619611518e2ba03c3fb2a
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2024-09-23 14:23:04 +0100

    Update and rename C2ME-GCN-Cora.py to C3E-GCN-Cora.py

--- Train_Val_Test/C3E-GCN-Cora.py
+++ Train_Val_Test/C3E-GCN-Cora.py
@@ -5,65 +5,48 @@ from torch_geometric.datasets import Planetoid
 from graph_nn_optimization import GraphNNOptimization, round_to_nearest_even
 
 
-torch.manual_seed(42) #0, 42, 3407
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-dname, root, sroot = 'Cora', '/home/onecs-casino/PycharmProjects/pythonProject/data', '/home/onecs-casino/PycharmProjects/pythonProject/saved_model_ICLR'
+dname, root = 'Cora', '/home/PycharmProjects/pythonProject/data'
 #================================================================================================================================================================
 dataset = Planetoid(root, name=dname, transform=T.AddSelfLoops())
 data = dataset[0]
-k, C, H = 1, torch.unique(data.y).shape[0], np.log(data.x.shape[0]+1)
+H = np.log(data.x.shape[0])
 Optimizer = GraphNNOptimization(data)
 Optimizer.optimize_weights(k, C, H, True)
-
 #================================================================================================================================================================
 print(f'Current dataset is {dname}:')
-#num_mmp_layer = [data.x.shape[1], 1982, 1300, 1092, 968, 766]
-#num_mmp_layer = [data.x.shape[1], 1982, 1298, 1094, 986, 722]
-num_mmp_layer = [data.x.shape[1], 1982, 1308, 1080, 947, 726]
+#num_mmp_layer = [data.x.shape[1], 1982, 1300, 1092, 968, 766] [data.x.shape[1], 1982, 1308, 1080, 947, 726]
+num_mmp_layer = [data.x.shape[1], 1982, 1298, 1094, 986, 722]
 num_postpro_layer = [num_mmp_layer[-1], torch.unique(data.y).shape[0]]
 num_skip_layer = num_mmp_layer[1:]
-#p = [0.5803914072580636, 0.3961135669506727, 0.4568605721739575, 0.46962138534848274, 0.4415475075395707-0.1]
-#p = [0.58, 0.395, 0.46, 0.47, 0.42]
+#p = [0.5803914072580636, 0.3961135669506727, 0.4568605721739575, 0.46962138534848274, 0.4415475075395707-0.1] [0.58, 0.395, 0.46, 0.47, 0.42] [0.586, 0.393, 0.456, 0.474, 0.4824]
 p= [0.5803915091280454, 0.39768745419779084, 0.4522683210408116, 0.4670709288239129, 0.4342272413012144]
 
-print(f'Current modified entropy is {np.log(data.x.shape[0] * k * C) + np.sum(np.log(num_mmp_layer[1:]))}')
-terms = []
-for i in range(1, len(num_mmp_layer)):
-    terms.append((1 / i) * np.log(num_mmp_layer[i - 1] * num_mmp_layer[i] / (num_mmp_layer[i - 1] + num_mmp_layer[i])))
-print(f'Current modified constraint is {sum(terms) - H}')
-
-#num_postpro_layer = [722, torch.unique(data.y).shape[0]]
-#num_skip_layer = [1982, 1298, 1094, 986, 722]
-#p = [0.586, 0.393, 0.456, 0.474, 0.4824]
-
-
-#num_mmp_layer = [data.x.shape[1], 512, 256, 128, 64, 32, 16]
-#num_postpro_layer = [16, torch.unique(data.y).shape[0]]
-#num_skip_layer = [512, 256, 128, 64, 32, 16]
-#p = [0.6, 0.6, 0.6, 0.6, 0.6, 0.6] # lr=2e-4, weight_decay=5e-4
+# Deep variant settings, i.e., simply stacking deeper with the width of original works
 #num_mmp_layer = [data.x.shape[1], 16, 16, 16, 16, 16]
 #num_postpro_layer = [16, torch.unique(data.y).shape[0]]
 #num_skip_layer = [16, 16, 16, 16, 16]
-#p = [0.6, 0.6, 0.6, 0.6, 0.6] #lr = 1e-4 wd= 1e-5 5-layer entropy:622.59 acc:0.830 +- 0.008
+#p = [0.6, 0.6, 0.6, 0.6, 0.6]
+#num_mmp_layer = [data.x.shape[1], 64, 64, 64, 64, 64]
+#num_postpro_layer = [64, torch.unique(data.y).shape[0]]
+#num_skip_layer = [64, 64, 64, 64, 64]
+#p = [0.6, 0.6, 0.6, 0.6, 0.6]
 #======================================================================================================================================
-
-
 data, flag, deep = data.to(device), 0, 0
-for runs in range(1):
+for runs in range(10):
 
     if flag == 1:
         break
+        
     best_test_acc = 0
     best_val_acc = 0
-    epochs = 0
-    flag = 0
-
+    epochsv, epochst = 0, 0
 
     model = AAGNN(num_mmp_layer, num_postpro_layer, num_skip_layer)
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.9e-4, weight_decay=5e-4) #lr=0.9e-4, weight_decay=5e-4
 
-    for epoch in range(50):
+    for epoch in range(90):
         loss = train(model, data, p, optimizer)
         valacc = val(model, data, p,)
         acc = test(model, data, p,)
@@ -75,17 +58,8 @@ for runs in range(1):
 
         if acc > best_test_acc:
             best_test_acc = acc
-            epochs = epoch
-
-        if best_test_acc >= 0.846:
-            torch.save(model, sroot+'/c2me-gcn-'+dname+'.pth')
-            flag = 1
-            break
-        if best_test_acc >= 0.764 and deep == 1:
-            torch.save(model, sroot+'/deep-gcn-'+dname+'.pth')
-            flag = 1
-            break
+            epochst = epoch
     print(f'Current Run={runs}')
     print(f'Best Val Accuracy={best_val_acc}, Epoch={epochsv}')
-    print(f'Best Test Accuracy={best_test_acc}, Epoch={epochs}')
+    print(f'Best Test Accuracy={best_test_acc}, Epoch={epochst}')
     print('========================')
