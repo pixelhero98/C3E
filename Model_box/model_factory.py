17e3bc35a40f317cb60e818315d23ea1e72dfbf7
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2025-06-17 17:08:05 +0100

    Update model_factory.py

--- Model_box/model_factory.py
+++ Model_box/model_factory.py
@@ -70,7 +70,7 @@ class Model(nn.Module):
                 )
             elif key == 'appnp':
                 self.propagation.append(
-                    Conv(prop_layer[i], prop_layer[i + 1], K=1, lambda_max=2.0, cached=True, bias=True)
+                    Conv(prop_layer[i], prop_layer[i + 1], K=10, alpha=0.1, dropout=0， cached=True, add_self_loops=True, normalize=True, bias=True)
                 )
             elif key == 'sgc':
                 self.propagation.append(
