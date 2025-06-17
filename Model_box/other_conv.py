84cf09c0660dc5feb5b0dc002f2d7b9a4d20ac70
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2025-06-17 14:47:30 +0100

    Update other_conv.py

--- Model_box/other_conv.py
+++ Model_box/other_conv.py
@@ -4,14 +4,6 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 
-class GDCPPR(nn.Module):
-   
-        return out
-
-class GDCHK(nn.Module):
-   
-        return out
-
 class JACOBIConv(nn.Module):
    
         return out
