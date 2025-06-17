eef187cffcce6aace26b6a09d6567e85626b8f58
pixelhero98 <116365512+pixelhero98@users.noreply.github.com>
2025-06-17 17:58:03 +0100

    Update c3e.py

--- c3e.py
+++ c3e.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import minimize
 from typing import Optional, List
 
-class CCCE:
+class ChanCapConEst:
     """
     Estimate propagation layers and their hidden dimensions for a graph neural network with C3E.
     """
